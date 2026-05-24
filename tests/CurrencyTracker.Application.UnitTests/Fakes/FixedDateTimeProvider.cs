7df43bbe2a0344b76728c356fdade8f37dcc0413
Marco Silva <marco.p.c.silva@gmail.com>
2026-05-24 09:12:16 +0200

    feat(4.1): Edit IDateTimeProvider port and SystemDateTimeProvider adapter

--- tests/CurrencyTracker.Application.UnitTests/Fakes/FixedDateTimeProvider.cs
+++ tests/CurrencyTracker.Application.UnitTests/Fakes/FixedDateTimeProvider.cs
@@ -7,7 +7,7 @@ namespace CurrencyTracker.Application.UnitTests.Fakes;
 /// </summary>
 public sealed class FixedDateTimeProvider : IDateTimeProvider
 {
-    private DateTimeOffset utcNow;
+    private DateTimeOffset _utcNow;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="FixedDateTimeProvider"/> class.
@@ -15,13 +15,13 @@ public sealed class FixedDateTimeProvider : IDateTimeProvider
     /// <param name="initialValue">The initial UTC date and time value.</param>
     public FixedDateTimeProvider(DateTimeOffset initialValue)
     {
-        utcNow = initialValue;
+        _utcNow = initialValue;
     }
 
     /// <summary>
     /// Gets the current fixed UTC date and time.
     /// </summary>
-    public DateTimeOffset UtcNow => utcNow;
+    public DateTimeOffset UtcNow => _utcNow;
 
     /// <summary>
     /// Moves the clock forward or backward by the provided delta.
@@ -29,7 +29,7 @@ public sealed class FixedDateTimeProvider : IDateTimeProvider
     /// <param name="delta">The amount of time to add.</param>
     public void Advance(TimeSpan delta)
     {
-        utcNow = utcNow.Add(delta);
+        _utcNow = _utcNow.Add(delta);
     }
 
     /// <summary>
@@ -38,6 +38,6 @@ public sealed class FixedDateTimeProvider : IDateTimeProvider
     /// <param name="moment">The UTC date and time to set.</param>
     public void Set(DateTimeOffset moment)
     {
-        utcNow = moment;
+        _utcNow = moment;
     }
 }
